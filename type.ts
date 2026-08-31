7c6896720d039ba9760241372375413ceb7a37ad
Rahul Kumar <rahulofficial3200@gmail.com>
2026-08-31 09:44:34 +0530

    first commit

---
+++ type.ts
@@ -0,0 +1,15 @@
+//this is Type inference ->TypeScript determines the type from the value
+const name="rahul";
+const age=20;
+const isLoading=true;
+console.log(name);
+console.log(age);
+console.log(isLoading);
+
+//this is Explicit type annotation-> You tell TypeScript the type:
+const userName:string ="rahul kunar";
+const roll:number=3200
+const isLoad:boolean=false;
+console.log(userName);
+console.log(roll);
+console.log(isLoad);
