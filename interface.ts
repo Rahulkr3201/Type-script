5212b3e26226750032f69a649715ca7a0464c352
Rahul Kumar <rahulofficial3200@gmail.com>
2026-09-04 17:09:52 +0530

    add initial implementation of add function and user object definition

---
+++ interface.ts
@@ -0,0 +1,12 @@
+//without interface we used to do
+let user:{
+    name:string;
+    age:number;
+    rollNo:number;
+    drink:boolean;
+}={
+    name:"rahul",
+    age:22,
+    rollNo:123,
+    drink:true
+}
